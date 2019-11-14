abdf79c80238ee8efb84c4384f34065d49f4391f
Can Berberoglu <huseyincan@4play.com.tr>
2019-11-14 22:40:13 +0300

    added grouped assertions with assertAll

--- src/test/java/io/can/unittestingdemo/test/_02/junitBasics/StudentTestWithBasicAssertions.java
+++ src/test/java/io/can/unittestingdemo/test/_02/junitBasics/StudentTestWithBasicAssertions.java
@@ -49,4 +49,50 @@ public class StudentTestWithBasicAssertions {
 
     }
 
+    @Test
+    @DisplayName("Test every student must have an id, name and surname with groupped asssertions")
+    void shouldCreateStudentWithIdNameAndSurnameGroupedAssertions() {
+
+        // assertAll -> assertion'larin gruplanmasini ve grup halinde calistirilmasini saglar.
+        // Parametre olarak Executable(functional interface) alir. Bu parametre herhangi calistiralacak bir kod parcasidir.
+        // Assertion'lari icerisine tek tek, collection olarak veya stream olarak verebiliriz.
+
+        // Assertion'larin organize edilmesini saglar. Tek bir assertion fail olsa bile hepsini calistirmaya devam eder.
+        // Bir grup icerisindeki tum fail olan assertion'lari gosterebilmektedir.
+
+        Student student = new Student("1", "can", "berberoglu");
+
+        // Gruplanmis assertion'larin hepsi calisacaktir.
+        assertAll("Student's name check",
+                () -> assertEquals("can", student.getName()),
+                () -> assertEquals("can", student.getName(), "Student's name"),
+                () -> assertNotEquals("cem", student.getName(), "Student's name"));
+
+        // Eger herhangi bir failure var ise yine de diger assertion'lar calistirilacaktir.
+        assertAll("Student's name character check",
+                () -> assertTrue(student.getName().startsWith("ca")),
+                () -> assertTrue(student.getName().startsWith("ca"), () -> "Student's name " + "starts with 'ca'"),
+                () -> assertFalse(() -> {
+                    Student student2 = new Student("2", "cem", "berberoglu");
+                    return student2.getName().startsWith("ca");
+                }, () -> "Student's name " + "starts with 'ce'"));
+
+        // dependent groups
+        // Farkli executable icerisindekiler birbirine bagimli (dependent) degildir.
+        assertAll("dependent assertions",
+                () -> {
+                    // Executable1
+                    Student student2 = new Student("2", "cem", "berberoglu");
+                    assertArrayEquals(new String[]{"can", "cem"}, Stream.of(student, student2).map(Student::getName).toArray());
+                },
+                () -> {
+                    // Executable2
+                    // Aynı executable icerisinde birden fazla assert varsa bunlar birbirine bagimlidir(dependent)
+                    Student student2 = new Student("2", "cem", "berberoglu");
+                    assertSame(student, student);
+                    assertNotSame(student, student2);
+                });
+
+    }
+
 }
